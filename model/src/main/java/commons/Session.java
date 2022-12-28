df56d7363a2b7dfaabfdf12c336e8bdb6fe03329
aleborgi <alexborgese01@gmail.com>
2022-12-28 22:42:34 +0100

    Creato il singleton Session.

---
+++ model/src/main/java/commons/Session.java
@@ -0,0 +1,25 @@
+package commons;
+
+import entities.Worker;
+
+public class Session {
+    private Worker worker;
+    private static Session instance;
+
+    private Session(){
+        ;
+    }
+
+    public static Session getInstance() {
+        if (instance == null){
+            instance = new Session();
+        }
+        return instance;
+    }
+    public static void invalidate(){
+        instance = null;
+    }
+    public void update(Worker worker){
+        this.worker = worker;
+    }
+}
