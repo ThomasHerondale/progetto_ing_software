a764afba9e756b2cd4a8aa70c1248f14c0a3e4af
aleborgi <alexborgese01@gmail.com>
2022-12-28 13:29:39 +0100

    -

--- view/src/main/java/com/example/view/SalaryScreen.java
+++ view/src/main/java/com/example/view/SalaryScreen.java
@@ -5,7 +5,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Group;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 
 public class SalaryScreen extends LoggedScreen {
