e8313fd8ac05f8b85ceeeab0f55c1866e2a98aa9
aleborgi <alexborgese01@gmail.com>
2022-12-30 11:25:56 +0100

    stylesheet per il menu a tendina abstention

--- view/src/main/java/com/example/view/ViewShiftsScreen.java
+++ view/src/main/java/com/example/view/ViewShiftsScreen.java
@@ -1,11 +1,18 @@
 package com.example.view;
 
+import commons.Session;
+import database.DBMSDaemon;
+import database.DBMSException;
+import entities.Shift;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.MenuButton;
 import javafx.scene.control.ScrollBar;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+
+import java.util.List;
 
 public class ViewShiftsScreen extends LoggedScreen {
 
@@ -26,19 +33,31 @@ public class ViewShiftsScreen extends LoggedScreen {
 
     @FXML
     private ScrollPane verticalPane;
+    @FXML
+    private MenuButton abstentionsMenu;
 
     private ShiftHandler shiftHandler;
     private AccountInfoHandler accountInfoHandler;
+    private List<Shift> shiftsList;
 
     public ViewShiftsScreen(ShiftHandler handler){
         this.shiftHandler = handler;
         accountInfoHandler = new AccountInfoHandler();
+        try {
+            DBMSDaemon.getInstance().getShiftsList(Session.getInstance().getWorker().getId());
+        } catch (DBMSException e) {
+            //TODO:
+        }
     }
 
     @FXML
     public void initialize(){
         super.initialize();
+        abstentionsMenu.getStylesheets().add(String.valueOf(getClass().getResource("css/AbstentionsMenuStyle.css")));
+        abstentionsMenu.getStyleClass().add("abstentionsMenu");
         synchroBar();
+        //insertAllShiftsCard(Session.getInstance().getWorker().getId(),
+        //        Session.getInstance().getWorker().getFullName(), shiftsList);
     }
 
     private void synchroBar() {
@@ -46,6 +65,22 @@ public class ViewShiftsScreen extends LoggedScreen {
         verticalBar.valueProperty().bindBidirectional(verticalPane.vvalueProperty());
         horizontalBar.valueProperty().bindBidirectional(verticalAndHorizonatalPane.hvalueProperty());
         horizontalBar.valueProperty().bindBidirectional(horizontalPane.hvalueProperty());
+    }
+    private void insertAllShiftsCard(String idWorker, String fullNameWorker, List<Shift> shiftsList){
+        Shift shift;
+        AnchorPane shiftCard;
+        for (int i=0; i<shiftsList.size(); i++){
+            shift = shiftsList.get(i);
+            shiftCard = createShiftCard(idWorker,fullNameWorker, shift);
+        }
+    }
+    private AnchorPane createShiftCard(String id, String fullName, Shift shift){
+        AnchorPane shiftCard = new AnchorPane();
+        shiftCard.setPrefHeight(87);
+        shiftCard.setPrefWidth(227);
+
+
+        return shiftCard;
     }
 
     @FXML
