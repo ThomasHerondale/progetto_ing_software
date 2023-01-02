f943a0722337e5b195aa23eb1360ffc044896541
aleborgi <alexborgese01@gmail.com>
2023-01-03 00:04:31 +0100

    Completata la SalaryScreen, sarebbe da testare.

--- view/src/main/java/com/example/view/SalaryScreen.java
+++ view/src/main/java/com/example/view/SalaryScreen.java
@@ -1,31 +1,62 @@
 package com.example.view;
 
-import database.DBMSDaemon;
-import entities.Worker;
+import commons.HoursRecap;
+import commons.Session;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Group;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+
+import java.util.Map;
 
 public class SalaryScreen extends LoggedScreen {
 
     @FXML
+    private Label IDLabel;
+
+    @FXML
     private Button backButton;
+
+    @FXML
+    private Label baseSalary;
+
+    @FXML
+    private Label nameLabel;
+
+    @FXML
+    private Label overtimeSalary;
+
+    @FXML
+    private Label parentalLeaveSalary;
+
+    @FXML
+    private Label surnameLabel;
+
+    @FXML
+    private Label totSalary;
 
     private final ViewSalaryHandler viewSalaryHandler;
     private final AccountInfoHandler accountInfoHandler;
+    private Map<HoursRecap, Double> salaryData;
 
-    public SalaryScreen(ViewSalaryHandler handler){
+    public SalaryScreen(Map<HoursRecap, Double> salaryData, ViewSalaryHandler handler){
         //mancano ancora delle cose
         //TODO:
         this.viewSalaryHandler = handler;
         accountInfoHandler = new AccountInfoHandler();
+        this.salaryData = salaryData;
     }
     @Override
     public void initialize(){
         super.initialize();
-        //TODO:
+        IDLabel.setText(Session.getInstance().getWorker().getId());
+        nameLabel.setText(Session.getInstance().getWorker().getName());
+        surnameLabel.setText(Session.getInstance().getWorker().getSurname());
+        baseSalary.setText(String.valueOf(salaryData.entrySet().iterator().next().getKey().ordinaryHours()));
+        overtimeSalary.setText(String.valueOf(salaryData.entrySet().iterator().next().getKey().overtimeHours()));
+        parentalLeaveSalary.setText(String.valueOf(salaryData.entrySet().iterator().next().getKey().parentalLeaveHours()));
+        totSalary.setText(totSalary.getText() + " " + salaryData.entrySet().iterator().next().getValue());
     }
     @FXML
     public void clickBack(ActionEvent event) {
