389f8606a2dafe570dfdab37c6ae51adff10c6aa
aleborgi <alexborgese01@gmail.com>
2023-01-04 12:16:30 +0100

    Registra ingresso da remoto implementato

--- view/src/main/java/com/example/view/RemoteRecordEntryPopup.java
+++ view/src/main/java/com/example/view/RemoteRecordEntryPopup.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class RemoteRecordEntryPopup {
@@ -15,7 +16,7 @@ public class RemoteRecordEntryPopup {
     private Button confirmButton;
 
     @FXML
-    private ComboBox<?> entryTimeBox;
+    private ComboBox<String> entryTimeBox;
 
     @FXML
     private Label shiftDate;
@@ -29,13 +30,21 @@ public class RemoteRecordEntryPopup {
     }
     @FXML
     public void initialize(){
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-        shiftDate.setText(shiftEntity.getDate().format(formatter));
+        DateTimeFormatter formatterDate = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        shiftDate.setText(shiftEntity.getDate().format(formatterDate));
+        LocalTime startTime = shiftEntity.getStartTime();
+        entryTimeBox.getItems().add(0, String.valueOf(startTime));
+        for (int i = 1; i < 7; i++){
+            startTime = startTime.plusMinutes(5);
+            entryTimeBox.getItems().add(i, String.valueOf(startTime));
+        }
     }
 
     @FXML
     void clickConfirm(ActionEvent event) {
-        //recordEntryHandler.clickedConfirm();
+        if (entryTimeBox.getValue() != null){
+            recordEntryHandler.clickedConfirm(LocalTime.parse(entryTimeBox.getValue()));
+        }
     }
 
 }
