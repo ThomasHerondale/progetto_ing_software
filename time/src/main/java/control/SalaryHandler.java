24a7072595ed9a5b6cdb43d46cc7f5595f5971be
thomas <thomasherondale@gmail.com>
2023-01-05 12:58:46 +0100

    aggiornamenti a TimerManager -> Cambio API Adattamento DBMSDAemon FUNZIONA

--- time/src/main/java/control/SalaryHandler.java
+++ time/src/main/java/control/SalaryHandler.java
@@ -1,4 +1,4 @@
-package com.example.control;
+package control;
 
 import commons.HoursRecap;
 import commons.Period;
@@ -15,20 +15,20 @@ public class SalaryHandler {
     /**
      * La tariffa oraria dei dipendenti.
      */
-    private static final double hourWage = 12.5;
+    private static final double HOUR_WAGE = 12.5;
     /**
      * Il fattore per cui saranno moltiplicate le tariffe relative allo straordinario.
      */
-    private static final double overtimeFactor = 1.5;
+    private static final double OVERTIME_FACTOR = 1.5;
     /**
      * Il fattore per cui saranno moltiplicate le tariffe relative al congedo parentale.
      */
-    private static final double parentalLeaveFactor = 0.7;
+    private static final double PARENTAL_LEAVE_FACTOR = 0.7;
 
     /**
      * Le gratifiche sullo stipendio per ogni livello.
      */
-    private static final Map<Character, Double> rankSalaries = Map.of(
+    private static final Map<Character, Double> RANK_SALARIES = Map.of(
             'H', 350.0,
             'A', 300.0,
             'B', 250.0,
@@ -39,7 +39,7 @@ public class SalaryHandler {
     /**
      * Lo stipendio di base di ogni impiegato.
      */
-    private static final Double baseSalary = 150.0;
+    private static final Double BASE_SALARY = 150.0;
 
     /* Costruttore privato per impedire l'istanziazione di questa classe */
     private SalaryHandler() {}
@@ -52,12 +52,12 @@ public class SalaryHandler {
      */
     private static double computeSalary(char rank, HoursRecap hours) {
         /* Calcola le tariffe orarie per gli straordinari e i congedi parentali */
-        var overtimeWage = hourWage * overtimeFactor;
-        var parentalLeaveWage = hourWage * parentalLeaveFactor;
+        var overtimeWage = HOUR_WAGE * OVERTIME_FACTOR;
+        var parentalLeaveWage = HOUR_WAGE * PARENTAL_LEAVE_FACTOR;
 
-        return baseSalary +
-                rankSalaries.get(rank) +
-                hours.ordinaryHours() * hourWage +
+        return BASE_SALARY +
+                RANK_SALARIES.get(rank) +
+                hours.ordinaryHours() * HOUR_WAGE +
                 hours.overtimeHours() * overtimeWage +
                 hours.parentalLeaveHours() * parentalLeaveWage;
     }
@@ -66,17 +66,12 @@ public class SalaryHandler {
      * Calcola lo stipendio di tutti i dipendenti, con riferimento al periodo specificato.
      * @param referencePeriod il periodo di riferimento
      */
-    public static void computeSalaries(Period referencePeriod) {
-        try {
-            var dbms = DBMSDaemon.getInstance();
-            Map<Worker, HoursRecap> salaryData = dbms.getWorkersData(referencePeriod);
-            for (var workerInfo : salaryData.entrySet()) {
-                var salary = computeSalary(workerInfo.getKey().getRank(), workerInfo.getValue());
-                dbms.setSalary(workerInfo.getKey().getId(), referencePeriod.end(), salary);
-            }
-        } catch (DBMSException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
+    public static void computeSalaries(Period referencePeriod) throws DBMSException {
+        var dbms = DBMSDaemon.getInstance();
+        Map<Worker, HoursRecap> salaryData = dbms.getWorkersData(referencePeriod);
+        for (var workerInfo : salaryData.entrySet()) {
+            var salary = computeSalary(workerInfo.getKey().getRank(), workerInfo.getValue());
+            dbms.setSalary(workerInfo.getKey().getId(), referencePeriod.end(), salary);
         }
     }
 }
--- time/src/main/java/timer/TimerManager.java
+++ time/src/main/java/timer/TimerManager.java
@@ -1,10 +1,16 @@
 package timer;
 
+import commons.Period;
+import control.SalaryHandler;
+import control.ShiftProposalHandler;
 import database.DBMSDaemon;
 import database.DBMSException;
 
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Date;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -13,18 +19,17 @@ public class TimerManager {
      * Il timer che si occupa delle uscite automatiche.
      */
     private final Timer autoExitTimer;
+
     /**
-     * Il timer che si occupa del reset dei contatori dei dipendenti.
+     * La data di inizio di ogni trimestre.
      */
-    private MonthlyTimer resetCountersTimer;
-    /**
-     * Il timer che si occupa del calcolo degli stipendi.
-     */
-    private QuarterlyTimer newSalariesTimer;
-    /**
-     * Il timer che si occupa del calcolo della nuova proposta di turnazione.
-     */
-    private QuarterlyTimer shiftProposalTimer;
+    private static final List<LocalDate> firstDaysOfQuarters = List.of(
+            LocalDate.of(2023, 1, 2),
+            LocalDate.of(2023, 4, 3),
+            LocalDate.of(2023, 7, 3),
+            LocalDate.of(2023, 10, 2)
+    );
+
     /**
      * L'intervallo tra una registrazione di uscite automatiche e un'altra.
      */
@@ -64,17 +69,18 @@ public class TimerManager {
      * Inizializza il manager avviando tutti task.
      */
     public void initialize() {
+        /* Programma il timer delle uscite automatiche ogni mezz'ora */
         var rate = debugMode ? 10_000 : AUTO_EXIT_RATE;
+        var currentDate = debugMode ?
+                LocalDate.of(2023, 1, 27)
+                :
+                LocalDate.now();
+        var currentTime = debugMode ?
+                LocalTime.of(23, 30)
+                :
+                LocalTime.now();
+
         autoExitTimer.scheduleAtFixedRate(new AutoExitTask(debugMode), Date.from(Instant.now()), rate);
-        this.resetCountersTimer = MonthlyTimer.schedule(
-                () -> {
-                    try {
-                        DBMSDaemon.getInstance().resetCounters();
-                    } catch (DBMSException e) {
-                        if (debugMode)
-                            System.err.println("[DEBUG - RESET_COUNTERS - DBMS ERROR]");
-                    }
-                }, 27, 23);
 
         /* Task di debug */
         if (debugMode) {
@@ -85,6 +91,60 @@ public class TimerManager {
                 }
             }, Date.from(Instant.now()), 5_000);
         }
+
+        /* Controlla la data per proporre una nuova turnazione */
+        for (var firstDay : firstDaysOfQuarters) {
+            var shiftProposalDay = firstDay.minusWeeks(1);
+            if (currentDate.equals(shiftProposalDay))
+                newShiftProposal(firstDay);
+        }
+
+        /* Controlla la data per calcolare gli stipendi e resettare i contatori*/
+        /* Se è il 27 di un mese, e sono passate le 23 */
+        if (currentDate.getDayOfMonth() == 27 &&
+                currentTime.isAfter(LocalTime.of(23, 0))) {
+            if (debugMode)
+                System.err.println("[DEBUG - SALARIES - ACTION]...");
+            newSalaries(currentDate);
+            if (debugMode)
+                System.err.println("[DEBUG - SALARIES - ACTION]");
+            resetCounters();
+            if (debugMode)
+                System.err.println("[DEBUG - RESET_COUNTERS - ACTION]");
+        }
+    }
+
+    private void resetCounters() {
+        try {
+            DBMSDaemon.getInstance().resetCounters();
+        } catch (DBMSException e) {
+            throw new TimerException(e.getMessage());
+        }
+    }
+
+    private void newShiftProposal(LocalDate firstDayOfQuarter) {
+        try {
+            var workers = DBMSDaemon.getInstance().getWorkersList();
+            var holidays = DBMSDaemon.getInstance().getRequestedHolidays(firstDayOfQuarter);
+            var handler = new ShiftProposalHandler(firstDayOfQuarter, workers, holidays);
+            handler.computeNewShiftsProposal();
+        } catch (DBMSException e) {
+            throw new TimerException(e.getMessage());
+        }
+    }
+
+    private void newSalaries(LocalDate endDate) {
+        System.out.println("End: " + endDate);
+        var referencePeriod = new Period(
+                endDate.minusMonths(1),
+                endDate.minusDays(1)
+        );
+        try {
+            SalaryHandler.computeSalaries(referencePeriod);
+        } catch (DBMSException e) {
+            e.printStackTrace();
+            throw new TimerException(e.getMessage());
+        }
     }
 
     /**
@@ -93,7 +153,6 @@ public class TimerManager {
     public void cancel() {
         autoExitTimer.cancel();
         autoExitTimer.purge();
-        resetCountersTimer.cancelCurrent();
         invalidate();
     }
 
