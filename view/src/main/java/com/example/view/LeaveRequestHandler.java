729dfd16556a2ea5c64222f9f873e1d589325e4e
aleborgi <alexborgese01@gmail.com>
2023-01-08 13:10:56 +0100

    Richiesta permesso terminata, manca modifica turnazione per completarla

--- view/src/main/java/com/example/view/LeaveRequestHandler.java
+++ view/src/main/java/com/example/view/LeaveRequestHandler.java
@@ -6,6 +6,8 @@ import database.DBMSException;
 import entities.Shift;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,6 +27,20 @@ public class LeaveRequestHandler {
             }
         }
         NavigationManager.getInstance().createPopup("Leave Request",
-                controller -> LeaveRequestPopup(realShiftList, this));
+                controller -> new LeaveRequestPopup(realShiftList, this));
+    }
+
+    public List<String> computeTimeLock(LocalTime startTime, LocalTime endTime) {
+        List<String> returnList = new ArrayList<>();
+        while (startTime != endTime){
+            returnList.add(startTime.format(DateTimeFormatter.ofPattern("HH:mm")));
+            startTime = startTime.plusHours(1);
+        }
+        return returnList;
+    }
+
+    public void clickedConfirm(String startTime, String endTime) {
+        //TODO: invoca il caso d'uso modifica turnazione
+        NavigationManager.getInstance().closePopup("Leave Request");
     }
 }
