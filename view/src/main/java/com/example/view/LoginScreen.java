88d468349e60bd9572011daaf1c5eb28e88018dc
aleborgi <alexborgese01@gmail.com>
2023-01-11 12:23:47 +0100

    Logo e icona inseriti

--- view/src/main/java/com/example/view/LoginScreen.java
+++ view/src/main/java/com/example/view/LoginScreen.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class LoginScreen {
